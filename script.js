d99a96aaf406a947f2a4a4ccc026423b1b237579
Heloisevikc <heloise.anders@escola.pr.gov.br>
2025-02-20 20:09:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let audios = [
-    {caminho:'menamora.mp3', legenda:'Matuto de verdade'},
+    {caminho:'menamora.mp3', legenda:'Me Namora'},
     {caminho:'Arash.mp3', legenda:'Arash'},
     {caminho:'Paula Fernandes.mp3', legenda:'Paula Fernandes'},
     {caminho:'Hayit.mp3', legenda:'Hayit'},
